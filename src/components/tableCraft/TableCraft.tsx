c797fb8fba5a26f556f146e7e7ab576991b0b76c
juven0 <bocquelin@gmail.com>
2024-11-20 14:56:18 +0100

    refont

---
+++ src/components/tableCraft/TableCraft.tsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+const TableCraft = ():JSX.Element=>{
+    return(<><h1>table craft</h1></>)
+}
+
+export default TableCraft
---
+++ src/index.ts
@@ -0,0 +1,1 @@
+export {default as TableCraft} from "./components/tableCraft/TableCraft"
