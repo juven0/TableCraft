48533958b1628121e6404dda948cffffa0bdf929
juven0 <bocquelin@gmail.com>
2024-11-22 07:43:51 +0300

    pk

---
+++ src/index.ts
@@ -0,0 +1,1 @@
+export { default as TableCraft } from "./tableCraft/TableCraft";
---
+++ src/tableCraft/TableCraft.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const TableCraft = (): JSX.Element => {
+  return (
+    <>
+      <h1>TableCraft</h1>
+    </>
+  );
+};
+
+export default TableCraft;
